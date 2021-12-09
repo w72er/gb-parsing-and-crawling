45168b6167a21807b7af1542eedc312473a4ac2a
w72er <>
2021-12-09 16:30:55 +0700

    lesson 5 hw

--- lesson-5/hw/scraper.py
+++ lesson-5/hw/scraper.py
@@ -2,122 +2,16 @@
 # Написать программу, которая собирает входящие письма из своего или тестового
 # почтового ящика и сложить данные о письмах в базу данных
 # (от кого, дата отправки, тема письма, текст письма полный)
-import time
-from pprint import pprint
-
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
-def login(driver: webdriver.Chrome):
-    driver.get('https://mail.ru/')
-    element = driver.find_element(By.CSS_SELECTOR, 'input[name=login]')
-    element.send_keys('study.ai_172')
-    element = driver.find_element(By.CSS_SELECTOR, 'button.button')
-    element.click()
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[name=password]'))
-    )
-    element.send_keys('NextPassword172#')
-    element = driver.find_element(By.CSS_SELECTOR, 'button.second-button')
-    element.click()
-
-
-def get_next_element(href):
-    elements = WebDriverWait(driver, 20).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.llc'))
-    )
-    i = 0
-    for element in elements:
-        i_href = element.get_attribute('href')
-        if i_href == href:
-            return elements[i + 1] if i < len(elements) - 1 else None
-        i += 1
-
-
-def parse_emails(driver: webdriver.Chrome):
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'a.llc'))
-    )
-    i = 0
-    while element:
-        href = element.get_attribute('href')
-        # element.click()
-        # email = get_email(driver)
-        # print(f'{email["date"]}: {email["subject"]}')
-        # driver.back()
-        print(f'i: {i}')
-        i += 1
-
-        webdriver.ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
-        element = get_next_element(href)
-
-
-def get_email(driver: webdriver.Chrome):
-    # от кого, дата отправки, тема письма, текст письма полный
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'span.letter-contact'))
-    )
-    contact = element.text
-    element = driver.find_element(By.CSS_SELECTOR, '.letter__date')
-    date = element.text
-    element = driver.find_element(By.CSS_SELECTOR, '.thread__subject')
-    subject = element.text
-    element = driver.find_element(By.CSS_SELECTOR, '.letter__body')
-    body = element.text
-    return {
-        'contact': contact,
-        'date': date,
-        'subject': subject,
-        'body': body
-    }
-
-
-def get_email_id_from_email_url(url: str) -> str:
-    return url.split('/')[4]
-
-
-def get_all_email_links(driver: webdriver.Chrome):
-    links = set()
-    last_href = None
-    while True:
-        elements = WebDriverWait(driver, 20).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.llc'))
-        )
-
-        for element in elements:
-            links.add(element.get_attribute('href'))
-
-        href = elements[-1].get_attribute('href')
-        if last_href == href:
-            print('DONE')
-            break
-        else:
-            last_href = href
-
-        actions = ActionChains(driver)
-        actions.move_to_element(elements[-1])
-        actions.perform()
-
-    return links
+from mail_ru_emails_parser import MailRuEmailsParser
 
 
 if __name__ == '__main__':
-    # https://chromedriver.chromium.org/downloads
-    driver = webdriver.Chrome('./chromedriver.exe')
-    login(driver)
-    links = get_all_email_links(driver)
-    links = list(links)
-    print(f'len: {len(links)}')
-    for link in links[:4]:
-        email_id = get_email_id_from_email_url(link)
-        driver.get(link)
-        email = get_email(driver)
-        email['id'] = email_id
-        print(f'{email["id"]} {email["date"]} {email["contact"]} {email["subject"][:30]}')
-    driver.quit()
-
+    with MailRuEmailsParser() as parser:
+        parser.login()
+        links = parser.get_all_email_links()
+        print(f'len: {len(links)}')
+        for link in links[:4]:
+            email_id = parser.get_email_id_from_email_url(link)
+            email = parser.get_email(link)
+            print(f'{email["id"]} {email["date"]} {email["contact"]} {email["subject"][:30]}')
+    print('with done')
