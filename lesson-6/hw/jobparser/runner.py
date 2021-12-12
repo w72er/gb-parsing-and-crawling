ac79aaa433059960537c6279affe0029d8d08a0d
w72er <>
2021-12-12 16:28:00 +0700

    simplify runner.py

--- lesson-6/hw/jobparser/runner.py
+++ lesson-6/hw/jobparser/runner.py
@@ -3,6 +3,7 @@
 """
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
+from scrapy.utils.project import get_project_settings
 
 from jobparser import settings
 from jobparser.spiders.hhru import HhruSpider
@@ -10,10 +11,7 @@ from jobparser.spiders.hhru import HhruSpider
 
 
 if __name__ == '__main__':
-    crawler_settings = Settings()
-    crawler_settings.setmodule(settings)
-
-    process = CrawlerProcess(settings=crawler_settings)
+    process = CrawlerProcess(get_project_settings())
     process.crawl(HhruSpider)
     # process.crawl(SuperjobSpider)
 
