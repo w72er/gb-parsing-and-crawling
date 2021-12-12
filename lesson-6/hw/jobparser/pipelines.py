1290bf18faa11ae6a116ad9f688932755bb4330a
w72er <>
2021-12-12 22:47:51 +0700

    process superjob items

--- lesson-6/hw/jobparser/pipelines.py
+++ lesson-6/hw/jobparser/pipelines.py
@@ -16,9 +16,13 @@ class JobparserPipeline:
         self.mongobase = client.lesson6hw
 
     def process_item(self, item, spider):
-        item['min'], item['max'], item['cur'] = self.process_salary(item['salary'])
-        # del item['salary']
-        # if spider.name == 'sjru':
+        if spider.name == 'hhru':
+            item['min'], item['max'], item['cur'] = self.process_salary_hhru(item['salary'])
+            item['_id'] = 'hhru: ' + item['link'].split('?')[0].split('/')[-1]
+        elif spider.name == 'superjobru':
+            item['min'], item['max'], item['cur'] = self.process_salary_superjobru(item['salary'])
+            item['_id'] = 'superjobru: ' + item['link'].split('/')[-1]
+        del item['salary']
 
         collection = self.mongobase[spider.name]
         collection.insert_one(dict(item))
@@ -28,18 +32,40 @@ class JobparserPipeline:
     def transform_salary(salary: str) -> int:
         return int(salary.replace('\xa0', ''))
 
-    @staticmethod
-    def process_salary(salary):
+    def process_salary_superjobru(self, salary):
+        # ['от', '\xa0', '360\xa0000\xa0руб.', '/', 'месяц']
+        # ['до', '\xa0', '150\xa0000\xa0руб.', '/', 'месяц']
+        # ['По договорённости']
+        # ['30\xa0000', '\xa0', '—', '\xa0', '35\xa0000', '\xa0', 'руб.', '/', 'месяц']
+        if salary[0] == 'По договорённости':
+            return None, None, None
+
+        if salary[0] == 'от':
+            frm = int(str.join('', salary[2].split('\xa0')[:-1]))
+            cur = salary[2].split('\xa0')[-1]
+            return frm, None, cur
+
+        if salary[0] == 'до':
+            to = int(str.join('', salary[2].split('\xa0')[:-1]))
+            cur = salary[2].split('\xa0')[-1]
+            return None, to, cur
+
+        if salary[2] == '—':
+            return self.transform_salary(salary[0]), self.transform_salary(salary[4]), salary[6]
+            return salary[1], None, salary[3]
+        pass
+
+    def process_salary_hhru(self, salary):
         if salary[0] == 'з/п не указана':
             return None, None, None
 
         if salary[2] == ' до ':
-            return salary[1], salary[3], salary[5]
+            return self.transform_salary(salary[1]), self.transform_salary(salary[3]), salary[5]
 
         if salary[0] == 'от ':
-            return salary[1], None, salary[3]
+            return self.transform_salary(salary[1]), None, salary[3]
 
         if salary[0] == 'до ':
-            return salary[1], None, salary[3]
+            return None, self.transform_salary(salary[1]), salary[3]
 
         return None, None, None
--- lesson-6/hw/jobparser/runner.py
+++ lesson-6/hw/jobparser/runner.py
@@ -2,18 +2,16 @@
  Ранер позволяет запускать скрапер в режиме отладки
 """
 from scrapy.crawler import CrawlerProcess
-from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
 
-from jobparser import settings
 from jobparser.spiders.hhru import HhruSpider
-# from jobparser.spiders.superjob import SuperjobSpider
+from jobparser.spiders.superjobru import SuperjobruSpider
 
 
 if __name__ == '__main__':
     process = CrawlerProcess(get_project_settings())
     process.crawl(HhruSpider)
-    # process.crawl(SuperjobSpider)
+    process.crawl(SuperjobruSpider)
 
     process.start()
 
