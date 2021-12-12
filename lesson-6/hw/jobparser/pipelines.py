c32f717a00efaf6bd3206fa1cf33639780b06293
w72er <>
2021-12-12 19:48:55 +0700

    process items

--- lesson-6/hw/jobparser/pipelines.py
+++ lesson-6/hw/jobparser/pipelines.py
@@ -6,8 +6,40 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from pymongo import MongoClient
 
 
 class JobparserPipeline:
+
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.mongobase = client.lesson6hw
+
     def process_item(self, item, spider):
+        item['min'], item['max'], item['cur'] = self.process_salary(item['salary'])
+        # del item['salary']
+        # if spider.name == 'sjru':
+
+        collection = self.mongobase[spider.name]
+        collection.insert_one(dict(item))
         return item
+
+    @staticmethod
+    def transform_salary(salary: str) -> int:
+        return int(salary.replace('\xa0', ''))
+
+    @staticmethod
+    def process_salary(salary):
+        if salary[0] == 'з/п не указана':
+            return None, None, None
+
+        if salary[2] == ' до ':
+            return salary[1], salary[3], salary[5]
+
+        if salary[0] == 'от ':
+            return salary[1], None, salary[3]
+
+        if salary[0] == 'до ':
+            return salary[1], None, salary[3]
+
+        return None, None, None
--- lesson-6/hw/jobparser/spiders/hhru.py
+++ lesson-6/hw/jobparser/spiders/hhru.py
@@ -6,7 +6,10 @@ from jobparser.items import JobparserItem
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['https://novosibirsk.hh.ru/search/vacancy?clusters=true&area=4&ored_clusters=true&enable_snippets=true&salary=&text=Scala&from=suggest_post']
+    start_urls = [
+        # 'https://novosibirsk.hh.ru/search/vacancy?clusters=true&area=4&ored_clusters=true&enable_snippets=true&salary=&text=Scala&from=suggest_post',
+        'https://novosibirsk.hh.ru/search/vacancy?clusters=true&area=4&ored_clusters=true&enable_snippets=true&salary=&text=%D0%98%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80+%D0%BF%D0%BE+%D0%B0%D0%B2%D1%82%D0%BE%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8E%2FQA+Automation'
+    ]
 
     def parse(self, response: HtmlResponse, **kwargs):
         next_page = response.xpath('//a[@data-qa="pager-next"]/@href').get()
