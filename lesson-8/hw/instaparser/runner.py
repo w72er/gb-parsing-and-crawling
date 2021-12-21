ded105196d94416b4ac7e68c9f1f8b793d27d36d
Alexander Makarevich <alexander.makarevich@eltex.loc>
2021-12-21 09:52:09 +0700

    lesson-8

--- lesson-8/hw/instaparser/runner.py
+++ lesson-8/hw/instaparser/runner.py
@@ -1,14 +1,12 @@
 from scrapy.crawler import CrawlerProcess
-from scrapy.settings import Settings
+from scrapy.utils.project import get_project_settings
 
+from instaparser.spiders.insta_hw import InstaHwSpider
 from instaparser.spiders.instagram import InstaSpider
-from instaparser import settings
+
 
 if __name__ == '__main__':
-    crawler_settings = Settings()
-    crawler_settings.setmodule(settings)
-
-    process = CrawlerProcess(settings=crawler_settings)
+    process = CrawlerProcess(get_project_settings())
     process.crawl(InstaSpider)
-
+    process.crawl(InstaHwSpider, usernames=['pakjimin47', 'vasilisa_vasyx.x.x', 'sharapgplieva24'])
     process.start()
