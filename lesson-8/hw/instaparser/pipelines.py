1a984fa9a3c7c52406e008aa24fda1037c0dc078
w72er <>
2021-12-27 22:20:19 +0700

    lesson-8-hw part 1

--- lesson-8/hw/instaparser/pipelines.py
+++ lesson-8/hw/instaparser/pipelines.py
@@ -9,19 +9,7 @@ from itemadapter import ItemAdapter
 from users_db import UsersDb
 
 
-# class InstaparserPipeline:
-#     def process_item(self, item, spider):
-#         with UsersDb() as users_db:
-#             user_a = item['user_a']
-#             users_db.add(user_a)
-#             users_db.add_relation(user_a, item['following_users'], 'following')
-#             users_db.add_relation(user_a, item['follower_users'], 'followers')
-#         return item
-
-
 class InstaparserPipeline:
-    collection_name = 'scrapy_items'
-
     def __init__(self):
         self.users_db = UsersDb()
 
